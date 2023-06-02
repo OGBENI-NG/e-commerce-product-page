d2b495698c7a217ff833f02ba94caf53027c50b4
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-06-02 19:59:40 +0100

    remove-codes

--- data.js
+++ data.js
@@ -1,13 +1,17 @@
+import img1 from "./img/image-product-1.jpg"
+import img2 from "./img/image-product-2.jpg"
+import img3 from "./img/image-product-3.jpg"
+import img4 from "./img/image-product-4.jpg"
 const data = [{
     id: 1,
     name: "Fall Limited Edition Sneakers",
     description: "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
     price: 125.00,
     img: [
-        "../img/image-product-1.jpg",
-        "../img/image-product-2.jpg",
-        "../img/image-product-3.jpg",
-        "../img/image-product-4.jpg"
+        img1,
+        img2,
+        img3,
+        img4
     ]
 }]
 
